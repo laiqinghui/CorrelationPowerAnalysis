7a689c5cbb2f347bae8a1f8d7736c5e8785cdf54
Lai Qing Hui <qinghui1994@gmail.com>
2019-04-10 22:49:17 +0800

    Clean up codes. Added option for max tracepoints to process

--- Main.py
+++ Main.py
@@ -8,67 +8,37 @@ from CPA import CPA
 def main(argv):
 
     # inputfilename, startpt, endpt = getInputsArgv(argv)
-    inputfilename, startpt, endpt = getInputs()
+    inputfilename, startpt, endpt, numTraces = getInputs()
     print("Processing ", inputfilename, "...")
+    print()
     pt, ct, tracesPoints = processCSV(inputfilename, startpt, endpt)
-    VisualizeTracesDifferences(pt, ct, tracesPoints)
-    # keysize = 16
-    #
-    #
-    # # Init CPA object
-    # cpa = CPA(keysize, len(traceSets))
-    #
-    #
-    # for tracesPointsLen in range(100, 110, 10):
-    #
-    #     # Set data points
-    #     cpa.SetTracesPointsAndPT(tracesPoints[:tracesPointsLen], pt[:tracesPointsLen])
-    #     # Do CPA
-    #     print("Doing Correlational Power Aalysis...")
-    #     key = cpa.Analyse()
-    #     # cpa.VisualizeCorrSingle(cpa.GetMatrixRelations()[0][0], cpa.GetMatrixRelations()[0][1])
-    #
-    #     print("Key(Hex): ", key)
-    #     print("Key(Text): ", bytearray.fromhex(str(key)).decode())
+    if numTraces == -1:
+        VisualizeTracesDifferences(pt, ct, tracesPoints)
+    else:
+        VisualizeTracesDifferences(pt, ct, tracesPoints[:numTraces])
 
 
-def VisualizeTracesDifferences(pt, ct, tracesPoints):
+def VisualizeCorrSingle(matrixRelation):
 
-    keysize = 16
-    traceSets = range(10, 350, 10)
-    cpaObjs = []
-    # Will contain temp 16 sets of 20 (256 by 1) matrices for concat
-    matrixRelationMatList = np.empty(shape=(keysize, len(traceSets)), dtype=object)
+    fig, axs = plt.subplots(1, 1)
+    axs.stem(matrixRelation)
+    axs.grid()
+    # axs.stem(index, matrixRelation[index])
+    axs.stem([abs(matrixRelation).argmax()], [matrixRelation[abs(matrixRelation).argmax()]], linefmt="C1-", markerfmt="C1o")
+
+    axs.set_xlabel('All possible sub-key guesses (Byte 0)')
+    axs.set_ylabel('Correlation Value')
+    plt.show()
+
+def VisualizeCorrAll(traceSets, matrixRelationMatList, keysize):
+
     # Will be size of 16
     ylistContainer = []
-
-
-    for index1, tracesPointsLen in enumerate(traceSets):
-        print("Working with ", tracesPointsLen, " traces now...")
-        cpa = CPA(keysize)
-        # Set data points
-        cpa.SetTracesPointsAndPT(tracesPoints[:tracesPointsLen], pt[:tracesPointsLen])
-        # Do CPA
-        print("Doing Correlational Power Aalysis...")
-        key = cpa.Analyse()
-        print("Key recovered with ", tracesPointsLen, " traces is ", [hex(k)+"|"+chr(k) for k in key])
-        # Size of 16
-        currentMatrixRelations = cpa.GetMatrixRelations()
-        # Populate matrix data for plotting later
-        for index2, eachByteRelation in enumerate(currentMatrixRelations):
-            # Tranpose from 1 x 256 to 256 x1
-            matrixRelationMatList[index2][index1] = (np.array(eachByteRelation).reshape(len(eachByteRelation), 1))
-
-
-        cpaObjs.append(cpa)
-
-    print("matrixRelationMatList.shape", matrixRelationMatList.shape)
 
     for m in matrixRelationMatList:
         # ylist will have a shape of (256 by 20)
         ylist = np.concatenate(m, axis=1)
         ylistContainer.append(ylist)
-
 
     # Plotting
     numOfRowsPerFig = 4
@@ -79,28 +49,69 @@ def VisualizeTracesDifferences(pt, ct, tracesPoints):
     figIndex = 0
     plotIndex = 1
     figures[figIndex] = plt.figure()
-    figures[figIndex].subplots_adjust(hspace=0.4, wspace=0.4)
+    figures[figIndex].subplots_adjust(hspace=0.7, wspace=0.7)
 
     for index, ylist in enumerate(ylistContainer):
         if index > numOfPlotsPerFig:
             figIndex += 1
             figures[figIndex] = plt.figure()
-            figures[figIndex].subplots_adjust(hspace=0.4, wspace=0.4)
+            figures[figIndex].subplots_adjust(hspace=0.7, wspace=0.7)
             plotIndex = 1
         ax = figures[figIndex].add_subplot(numOfRowsPerFig, numOfColPerFig, plotIndex)
         for y in ylist:
             ax.plot(traceSets, y)
-        ax.set_title(str(index))
+        ax.set_title('Key byte ' + str(index))
+        ax.set_xlabel('No. of traces')
+        ax.set_ylabel('Correlation Value')
         plotIndex += 1
 
-
     plt.show()
+
+
+
+def VisualizeTracesDifferences(pt, ct, tracesPoints):
+
+    keysize = 16
+    # print("Range count", (math.floor(len(tracesPoints)/10)*10)+10)
+    traceSets = range(10, (math.floor(len(tracesPoints)/10)*10)+10, 10)
+    cpaObjs = []
+    # Will contain temp 16 sets of 20 (256 by 1) matrices for concat
+    matrixRelationMatList = np.empty(shape=(keysize, len(traceSets)), dtype=object)
+
+    print("Starting CPA procedure with increasing number of traces now...")
+
+    for index1, tracesPointsLen in enumerate(traceSets):
+        print("Working with ", tracesPointsLen, " traces now...")
+        cpa = CPA(keysize)
+        # Set data points
+        cpa.SetTracesPointsAndPT(tracesPoints[:tracesPointsLen], pt[:tracesPointsLen])
+        # Do CPA
+        print("Doing Correlational Power Analysis...")
+        key = cpa.Analyse()
+        print("Key recovered with ", tracesPointsLen, " traces is ", [hex(k)+"|"+chr(k) for k in key])
+        # Size of 16
+        currentMatrixRelations = cpa.GetMatrixRelations()
+        # Populate matrix data for plotting later
+        for index2, eachByteRelation in enumerate(currentMatrixRelations):
+            # Tranpose from 1 x 256 to 256 x1
+            matrixRelationMatList[index2][index1] = (np.array(eachByteRelation).reshape(len(eachByteRelation), 1))
+
+        cpaObjs.append(cpa)
+
+    # print("matrixRelationMatList.shape", matrixRelationMatList.shape)
+
+    print("Plotting single sub-key byte correlation plot...")
+    VisualizeCorrSingle(cpaObjs[index1].GetMatrixRelations()[0])
+    print("Plotting all key bytes correlation plot...")
+    VisualizeCorrAll(traceSets, matrixRelationMatList, keysize)
+
 
 def getInputs():
 
     defaultCSV = "waveform374samples_327_2047.csv"
     defaultStart = 327
     defaultEnd = 2047
+    defaultNumTraces = -1
 
     print("Welcome to Power Analysis Tool!")
     print("===============================")
@@ -111,9 +122,11 @@ def getInputs():
     print("Start index: ", startpt)
     endpt = int(input("Please enter the end index (Press <enter> to use default end index): ") or defaultEnd)
     print("End index: ", endpt)
+    numTraces = int(input("Please enter the max no. of traces to be use for analysis.\nNo. of trace will start from 10 and end at this amount in steps of 10 (Press <enter> to use all traces available in the file): ") or defaultNumTraces)
+    print("No. of traces to use: ", numTraces if numTraces != -1 else 'All')
     print()
 
-    return inputfilename, startpt, endpt
+    return inputfilename, startpt, endpt, numTraces
 
 
 
@@ -155,8 +168,9 @@ def processCSV(filename, startpt, endpt):
     pt = dframe.iloc[:, 0:1]
     ct = dframe.iloc[:, 1:2]
     dataTraces = dframe.iloc[:, startpt + 1:endpt]
-    print("Processed data: ")
+    print("First 5 rows of processed data: ")
     print(dataTraces.head())
+    print()
 
     return pt.values, ct.values, dataTraces.values
 
